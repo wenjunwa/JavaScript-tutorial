4456b525868786e939b234566369ba4f27407765
wenjunscode <wenjunwang0508@gmail.com>
2024-10-06 08:30:01 -0500

    strings

---
+++ lesson-3/3.js
@@ -0,0 +1,42 @@
+//3a
+'My name is :';
+
+//3b
+'wenjun';
+
+//3c
+'My name is: ' + 'wenjun';
+
+//3d
+'Total cost: $' + (5 + 3);
+
+//3e
+`Total cost: $${5+3}`;
+
+//3f
+alert(`Total cost: $${5+3}`);
+
+//3g
+'Total cost:$' + (599+295)/100;
+
+//3h
+`Total cost:$${(599+295)/100}`;
+
+//3i
+alert(`Total cost:$${(599+295)/100}`);
+
+//3j
+alert(`Total cost:$${(599+295)/100}
+Thank you, come again`);
+
+//3k
+`Items (${2+2}): $${(2095 + 799)*2/100}`;
+
+//3l
+`Shipping & handling: $${(499+499)/100}`;
+
+//3m
+`Total before tax: ${(2095+799+499)/50}`;
+
+//3n
+`Estimated tax(10%): $${Math.round((2095+799+499)/5)/100}`;
